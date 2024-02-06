7ac06927e91f2e2d19ff54757814180960327021
athys <thys.angelo@gmail.com>
2024-02-06 14:35:41 +0000

    Add node class, Add tree class with prepareArray and buildTree methods

---
+++ script.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(this.prepareArray(array), 0, this.prepareArray(array).length - 1);
+  }
+
+  prepareArray(array) {
+    return [... new Set(array.toSorted((a, b) => a - b))];
+  }
+
+  buildTree(array, start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    return root;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let tree = new Tree(arr);
+prettyPrint(tree.root);
+// console.log(tree);
+// console.log(tree.root);
